3d38c4f427f34108e3ab7d4538d10b307220d67f
OlegH <oleg.horiakov@gmail.com>
2024-06-14 15:46:22 +0300

    Data provider

--- src/test/java/student/examples/CalculationServiceDBTest.java
+++ src/test/java/student/examples/CalculationServiceDBTest.java
@@ -3,12 +3,12 @@ package student.examples;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class CalculationServiceDBTest {
     private Connection connection;
@@ -17,40 +17,68 @@ public class CalculationServiceDBTest {
     public void setupService(){
         calculationService =new CalculationService();
     }
+
     @BeforeClass
     public void setupData() throws SQLException {
         //1. Open connection Postgre
         connection =  DriverManager.getConnection("jdbc:postgresql://192.168.222.129/calculation_service_test_data_db?user=postgres&password=qazwsx&ssl=false");
         Statement result = connection.createStatement();
-        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO test_data VALUES (1,?,?,1)");
+        result.executeUpdate("DELETE FROM test_data");
+        result.executeUpdate("DELETE FROM tests");
 
         //2.Insert test info into database.
-        result.executeUpdate("INSERT INTO tests VALUES (1, 'Sum of negatives', false, now())");
+        for (int j = 1; j <= 10 ; j++) {
+            int authorId = (int) Math.random()*4;
+            ResultSet resultSet = result.executeQuery("SELECT id FROM test_authors ORDER BY RANDOM() LIMIT 1");
+            resultSet.next();
 
-        //3. Insert test data
-        StringBuilder values = new StringBuilder();
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO test_data VALUES (" + j + ",?,?,"+j+")");
 
-        int summ = 0;
-        for (int i = 0; i < 10; i++) {
-            int value = (int) (Math.random() * (-999) -1);
-            summ += value;
-            values.append(value).append(",");
-        }
-        values.deleteCharAt(values.length() - 1);
-        String expected = String.format("%d", summ);
+            result.executeUpdate("INSERT INTO tests VALUES (" + j + ", 'Sum of negatives', false, now(), "+resultSet.getInt("id")+")");
+
+            //3. Insert test data
+            StringBuilder values = new StringBuilder();
+
+            int summ = 0;
+            for (int i = 0; i < 10; i++) {
+                int value = (int) (Math.random() * (-999) -1);
+                summ += value;
+                values.append(value).append(",");
+            }
+            values.deleteCharAt(values.length() - 1);
+            String expected = String.format("%d", summ);
 //        result.executeUpdate("INSERT INTO test_data VALUES")
-        preparedStatement.setString(1, values.toString() );
-        preparedStatement.setString(2, expected);
-        preparedStatement.executeUpdate();
+            preparedStatement.setString(1, values.toString() );
+            preparedStatement.setString(2, expected);
+            preparedStatement.executeUpdate();
+        }
+//        Fetching data
+    }
+    @DataProvider(name = "dp")
+    public Iterator<Map<String, Object>> provideData() throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM test_data ORDER BY (id)");
+        List<Map<String, Object>> testData = new ArrayList<>();
+        while (resultSet.next()){
+            Map<String, Object> sample = new HashMap<>();
+            sample.put("input", Arrays.stream(resultSet.getString("input").split(","))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList()));
+            sample.put("expected", resultSet.getInt("expected"));
+            testData.add(sample);
+        }
+        System.out.println(testData);
+        return testData.iterator();
     }
 
-
-    @Test
-    public void testSumOfLIst(){
+    @Test(dataProvider = "dp")
+    public void testSumOfIntegers(Map<String, Object> sample){
+        System.out.println("Test:::");
 
         Assert.assertEquals(calculationService.summ(new ArrayList<>(Arrays.asList(1, 2, 3, 4))), 10);
     }
     @AfterClass
+    //TODO: CLear table from database
     public void tearDown(){
         calculationService = null;
     }
